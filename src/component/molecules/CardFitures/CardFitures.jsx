87951f3eccc840733d8e6efed8bae5693a09c3e4
rakamahardika04 <raka.student04@gmail.com>
2023-02-07 09:53:37 +0700

    Memperbaiki Fitur Card-Fiture

--- src/component/molecules/CardFitures/CardFitures.jsx
+++ src/component/molecules/CardFitures/CardFitures.jsx
@@ -1,15 +1,36 @@
 import react from 'react';
 import './styles.css';
 
+import icon_brand_recognition from  '../../../assets/icons/icon-brand-recognition.svg';
+import icon_detailed_records from  '../../../assets/icons/icon-detailed-records.svg';
+import icon_fully_customizable from  '../../../assets/icons/icon-fully-customizable.svg';
+
 import CardFiture from '../../atoms/Cards/CardFiture/CardFiture';
 
 const CardFitures = () => {
   return (
     <div className="card-fitures-container">
       <span className="card-border" />      
-      <CardFiture />
-      <CardFiture />
-      <CardFiture />
+      <CardFiture 
+        card_image={icon_brand_recognition}
+        card_image_alt={'brand-recognition'}
+        card_headline={'Brand Recognition'}
+        card_subheadline={'Boost your brand recognition with each click. Generic links dont mean a thing. Branded links help instill confidence in your content.'}
+      />
+
+      <CardFiture 
+        card_image={icon_detailed_records}
+        card_image_alt={'detailed-records'}
+        card_headline={'Detailed Records'}
+        card_subheadline={'Gain insight into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.'}
+      />
+
+      <CardFiture 
+        card_image={icon_fully_customizable}
+        card_image_alt={'fully-customizable'}
+        card_headline={'Fully Customizable'}
+        card_subheadline={'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.'}
+      />
     </div>
   );
 }
--- src/component/organisms/Navbar/Navbar.jsx
+++ src/component/organisms/Navbar/Navbar.jsx
@@ -1,45 +1,48 @@
-import react from 'react';
+import react, { Component } from 'react';
 import BlackLogoButton from '../../atoms/Buttons/BlackLogo/BlackLogoButton';
 import LoginButton from '../../atoms/Buttons/Login/LoginButton';
 import SignUpButton from '../../atoms/Buttons/SignUp/SignUpButton';
 import './styles.css';
 
-const Navbar = () => {
-  return (
-    <nav class="navbar-section navbar navbar-expand-lg navbar-light bg-white">
-        <div class="container">
-            <div class="logo-container">
-                <BlackLogoButton />
-            </div>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+class Navbar extends Component {
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Features <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Resources</a>
-                    </li>
-                </ul>
+    render(){
+        return (
+            <nav class="navbar-section navbar navbar-expand-lg navbar-light bg-white">
+                <div class="container">
+                    <div class="logo-container">
+                        <BlackLogoButton />
+                    </div>
+                    <button onClick={this.handleToggler} class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-                <div class="nav-button form-inline my-2 my-lg-0">
-                         <div class="login-btn-container">
-                            <LoginButton />
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Features <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Resources</a>
+                            </li>
+                        </ul>
+
+                        <div class="nav-button form-inline my-2 my-lg-0">
+                                <div class="login-btn-container">
+                                    <LoginButton />
+                                </div>
+                                <div class='signup-btn-container'>
+                                    <SignUpButton />
+                                </div>
                         </div>
-                        <div class='signup-btn-container'>
-                            <SignUpButton />
-                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </nav>
-  );
+             </nav>
+        )
+    }
 }
 
 export default Navbar;
